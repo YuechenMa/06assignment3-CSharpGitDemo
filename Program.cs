95747b5234d91385d02261f029080460e549f306
Yuechen Ma <yuechenm@utas.edu.au>
2022-05-12 16:18:09 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06assignment3_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
